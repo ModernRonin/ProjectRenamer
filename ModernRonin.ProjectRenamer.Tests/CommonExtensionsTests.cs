46681943cbda84e624fede48dc99e1e449e80f6f
Mark Hajek <markhajek@outlook.com>
2021-04-05 11:30:12 +0100

    added first tests and activated NCrunch

---
+++ ModernRonin.ProjectRenamer.Tests/CommonExtensionsTests.cs
@@ -0,0 +1,15 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace ModernRonin.ProjectRenamer.Tests
+{
+    [TestFixture]
+    public class CommonExtensionsTests
+    {
+        [Test]
+        public void AsText_with_false_returns_no() => false.AsText().Should().Be("no");
+
+        [Test]
+        public void AsText_with_true_returns_yes() => true.AsText().Should().Be("yes");
+    }
+}
