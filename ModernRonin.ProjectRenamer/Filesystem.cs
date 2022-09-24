18eb2e11807a4536fb5b622da4b80dfc8433b8e5
Mark Hajek <markhajek@outlook.com>
2022-09-24 14:40:21 +0100

    made FileSystem.CurrentDirectory return the *absolute* path of the current directory

--- ModernRonin.ProjectRenamer/Filesystem.cs
+++ ModernRonin.ProjectRenamer/Filesystem.cs
@@ -1,24 +1,23 @@
 ﻿using System.IO;
 using System.Linq;
 
-namespace ModernRonin.ProjectRenamer
+namespace ModernRonin.ProjectRenamer;
+
+public class Filesystem : IFilesystem
 {
-    public class Filesystem : IFilesystem
-    {
-        public string CurrentDirectory => Directory.GetCurrentDirectory();
-        public bool DoesDirectoryExist(string directory) => Directory.Exists(directory);
-        public void EnsureDirectoryExists(string directory) => Directory.CreateDirectory(directory);
+    public string CurrentDirectory => Path.GetFullPath(Directory.GetCurrentDirectory());
+    public bool DoesDirectoryExist(string directory) => Directory.Exists(directory);
+    public void EnsureDirectoryExists(string directory) => Directory.CreateDirectory(directory);
 
-        public string[] FindProjectFiles(string directory, bool doRecurse, string projectFileExtension) =>
-            Directory
-                .EnumerateFiles(directory, $"*{projectFileExtension}", SearchOption(doRecurse))
-                .ToArray();
+    public string[] FindProjectFiles(string directory, bool doRecurse, string projectFileExtension) =>
+        Directory
+            .EnumerateFiles(directory, $"*{projectFileExtension}", SearchOption(doRecurse))
+            .ToArray();
 
-        public string[] FindSolutionFiles(string directory, bool doRecurse) =>
-            Directory.EnumerateFiles(".", $"*{Constants.SolutionFileExtension}", SearchOption(doRecurse))
-                .ToArray();
+    public string[] FindSolutionFiles(string directory, bool doRecurse) =>
+        Directory.EnumerateFiles(".", $"*{Constants.SolutionFileExtension}", SearchOption(doRecurse))
+            .ToArray();
 
-        static SearchOption SearchOption(bool doRecurse) =>
-            doRecurse ? System.IO.SearchOption.AllDirectories : System.IO.SearchOption.TopDirectoryOnly;
-    }
+    static SearchOption SearchOption(bool doRecurse) =>
+        doRecurse ? System.IO.SearchOption.AllDirectories : System.IO.SearchOption.TopDirectoryOnly;
 }
--- ModernRonin.ProjectRenamer/IFilesystem.cs
+++ ModernRonin.ProjectRenamer/IFilesystem.cs
@@ -1,11 +1,14 @@
-﻿namespace ModernRonin.ProjectRenamer
+﻿namespace ModernRonin.ProjectRenamer;
+
+public interface IFilesystem
 {
-    public interface IFilesystem
-    {
-        string CurrentDirectory { get; }
-        bool DoesDirectoryExist(string directory);
-        void EnsureDirectoryExists(string directory);
-        string[] FindProjectFiles(string directory, bool doRecurse, string projectFileExtension);
-        string[] FindSolutionFiles(string directory, bool doRecurse);
-    }
+    /// <summary>
+    ///     Returns the <b>absolute path</b> of the current directory.
+    /// </summary>
+    string CurrentDirectory { get; }
+
+    bool DoesDirectoryExist(string directory);
+    void EnsureDirectoryExists(string directory);
+    string[] FindProjectFiles(string directory, bool doRecurse, string projectFileExtension);
+    string[] FindSolutionFiles(string directory, bool doRecurse);
 }
