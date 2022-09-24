4bf1afbfc2dc3eee8a8a5d50aa47318b993388d6
Mark Hajek <markhajek@outlook.com>
2022-09-24 13:44:06 +0100

    switched Dotnet to use IToolRunner and completed its test coverage

--- ModernRonin.ProjectRenamer.Tests/DotnetTests.cs
+++ ModernRonin.ProjectRenamer.Tests/DotnetTests.cs
@@ -1,30 +1,110 @@
-﻿using AutofacContrib.NSubstitute;
+﻿using FluentAssertions;
 using NSubstitute;
 using NUnit.Framework;
 
-namespace ModernRonin.ProjectRenamer.Tests
+namespace ModernRonin.ProjectRenamer.Tests;
+
+[TestFixture]
+public class DotnetTests
 {
-    [TestFixture]
-    public class DotnetTests
+    [SetUp]
+    public void Setup()
     {
-        [SetUp]
-        public void Setup()
+        _underTest = new Dotnet(createRunner);
+
+        IToolRunner createRunner(string tool)
         {
-            _dependencies = new AutoSubstitute();
-            _underTest = _dependencies.Resolve<Dotnet>();
+            tool.Should().Be("dotnet");
+            _runner = Substitute.For<IToolRunner>();
+            return _runner;
         }
+    }
 
-        AutoSubstitute _dependencies;
-        Dotnet _underTest;
+    IToolRunner _runner;
+    Dotnet _underTest;
 
-        IExecutor Executor => _dependencies.Resolve<IExecutor>();
+    [Test]
+    public void AddReference()
+    {
+        // act
+        _underTest.AddReference("p1", "r1");
+        // assert
+        _runner.Received().Run("add \"p1\" reference \"r1\"");
+    }
 
-        [Test(Description = "fix for https://github.com/ModernRonin/ProjectRenamer/issues/24")]
-        public void AddToSolution_replaces_forward_slashes_with_backward_slashes_in_solutionFolder()
-        {
-            _underTest.AddToSolution("c:/myproject/myproject.csproj", "Features/.shared");
-            Executor.Received()
-                .Tool("dotnet", @"sln add -s ""Features\.shared"" ""c:/myproject/myproject.csproj""");
-        }
+    [Test]
+    public void AddToSolution_with_solutionFolder()
+    {
+        // act
+        _underTest.AddToSolution("c:/myproject/myproject.csproj", "Features/.shared");
+        // assert
+        _runner.Received().Run(@"sln add -s ""Features\.shared"" ""c:/myproject/myproject.csproj""");
+    }
+
+    [Test]
+    public void AddToSolution_without_solutionFolder()
+    {
+        // act
+        _underTest.AddToSolution("p1");
+        // assert
+        _runner.Received().Run("sln add \"p1\"");
+    }
+
+    [Test]
+    public void BuildSolution()
+    {
+        // arrange
+        // ReSharper disable once ConvertToLocalFunction
+        var onError = () => { };
+        // act
+        _underTest.BuildSolution(onError);
+        // assert
+        _runner.Received().Run("build", onError);
+    }
+
+    [Test]
+    public void GetReferencedProjects()
+    {
+        // arrange
+        _runner.RunAndGetOutput("list \"p1\" reference")
+            .Returns(@"
+Project reference(s)
+--------------------
+..\ModernRonin.ProjectRenamer\ModernRonin.ProjectRenamer.csproj
+.\SomeOther.csproj
+");
+        // act
+        var result = _underTest.GetReferencedProjects("p1");
+        // assert
+        result.Should()
+            .Equal("..\\ModernRonin.ProjectRenamer\\ModernRonin.ProjectRenamer.csproj",
+                ".\\SomeOther.csproj");
+    }
+
+    [Test]
+    public void PaketInstall()
+    {
+        // act
+        _underTest.PaketInstall();
+        // assert
+        _runner.Received().Run("paket install");
+    }
+
+    [Test]
+    public void RemoveFromSolution()
+    {
+        // act
+        _underTest.RemoveFromSolution("p1");
+        // assert
+        _runner.Received().Run("sln remove \"p1\"");
+    }
+
+    [Test]
+    public void RemoveReference()
+    {
+        // act
+        _underTest.RemoveReference("p1", "r1");
+        // assert
+        _runner.Received().Run("remove \"p1\" reference \"r1\"");
     }
 }
--- ModernRonin.ProjectRenamer/Dotnet.cs
+++ ModernRonin.ProjectRenamer/Dotnet.cs
@@ -2,54 +2,40 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace ModernRonin.ProjectRenamer
+namespace ModernRonin.ProjectRenamer;
+
+public class Dotnet : IDotnet
 {
-    public class Dotnet : IDotnet
-    {
-        const string ToolDotnet = "dotnet";
-        readonly IErrorHandler _errors;
-        readonly IExecutor _executor;
+    const string ToolDotnet = "dotnet";
+    readonly IToolRunner _runner;
 
-        public Dotnet(IExecutor executor, IErrorHandler errors)
-        {
-            _executor = executor;
-            _errors = errors;
-        }
+    public Dotnet(Func<string, IToolRunner> toolRunnerFactory) => _runner = toolRunnerFactory(ToolDotnet);
 
-        public void AddReference(string project, string reference) =>
-            ProjectReferenceCommand("add", project, reference);
+    public void AddReference(string project, string reference) =>
+        ProjectReferenceCommand("add", project, reference);
 
-        public void AddToSolution(string pathToProject, string solutionFolder) =>
-            SolutionCommand($"add -s {solutionFolder.ReplaceSlashesWithBackslashes().EscapeForShell()}",
-                pathToProject);
+    public void AddToSolution(string pathToProject, string solutionFolder) =>
+        SolutionCommand($"add -s {solutionFolder.ReplaceSlashesWithBackslashes().EscapeForShell()}",
+            pathToProject);
 
-        public void AddToSolution(string pathToProject) => SolutionCommand("add", pathToProject);
+    public void AddToSolution(string pathToProject) => SolutionCommand("add", pathToProject);
 
-        public void BuildSolution(Action onNonZeroExitCode) => Run("build", onNonZeroExitCode);
+    public void BuildSolution(Action onNonZeroExitCode) => _runner.Run("build", onNonZeroExitCode);
 
-        public IEnumerable<string> GetReferencedProjects(string project) =>
-            Read($"list {project.EscapeForShell()} reference")
-                .Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries)
-                .Skip(2);
+    public IEnumerable<string> GetReferencedProjects(string project) =>
+        _runner.RunAndGetOutput($"list {project.EscapeForShell()} reference")
+            .Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries)
+            .Skip(2);
 
-        public void PaketInstall() => Run("paket install");
-        public void RemoveFromSolution(string pathToProject) => SolutionCommand("remove", pathToProject);
+    public void PaketInstall() => _runner.Run("paket install");
+    public void RemoveFromSolution(string pathToProject) => SolutionCommand("remove", pathToProject);
 
-        public void RemoveReference(string project, string reference) =>
-            ProjectReferenceCommand("remove", project, reference);
+    public void RemoveReference(string project, string reference) =>
+        ProjectReferenceCommand("remove", project, reference);
 
-        void ProjectReferenceCommand(string command, string project, string reference) =>
-            Run($"{command} {project.EscapeForShell()} reference {reference.EscapeForShell()}");
+    void ProjectReferenceCommand(string command, string project, string reference) =>
+        _runner.Run($"{command} {project.EscapeForShell()} reference {reference.EscapeForShell()}");
 
-        string Read(string arguments) =>
-            _executor.ToolRead(ToolDotnet, arguments, () => _errors.Handle(ToolDotnet, arguments));
-
-        void Run(string arguments) => _executor.Tool(ToolDotnet, arguments);
-
-        void Run(string arguments, Action onNonZeroExitCode) =>
-            _executor.Tool(ToolDotnet, arguments, onNonZeroExitCode);
-
-        void SolutionCommand(string command, string projectPath) =>
-            Run($"sln {command} {projectPath.EscapeForShell()}");
-    }
+    void SolutionCommand(string command, string projectPath) =>
+        _runner.Run($"sln {command} {projectPath.EscapeForShell()}");
 }
