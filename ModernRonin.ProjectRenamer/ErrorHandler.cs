2c4cf9071ecfd6df8309f6e28b8d1041c4afa41e
Mark Hajek <markhajek@outlook.com>
2022-09-24 12:46:37 +0100

    cleanup

--- ModernRonin.ProjectRenamer/ErrorHandler.cs
+++ ModernRonin.ProjectRenamer/ErrorHandler.cs
@@ -1,39 +1,36 @@
 ﻿using System;
 
-namespace ModernRonin.ProjectRenamer
+namespace ModernRonin.ProjectRenamer;
+
+public class ErrorHandler : IErrorHandler
 {
-    public class ErrorHandler : IErrorHandler
+    readonly Lazy<IGit> _git;
+    readonly ILogger _logger;
+    readonly IRuntime _runtime;
+
+    public ErrorHandler(ILogger logger, Lazy<IGit> git, IRuntime runtime)
     {
-        readonly Lazy<IGit> _git;
-        readonly ILogger _logger;
-        readonly IRuntime _runtime;
+        _logger = logger;
+        _git = git;
+        _runtime = runtime;
+    }
 
-        public ErrorHandler(ILogger logger, Lazy<IGit> git, IRuntime runtime)
+    public void Handle(string msg) => Handle(msg, false);
+
+    public void Handle(string tool, string arguments) =>
+        Handle($"call '{tool} {arguments}' failed - aborting", true);
+
+    public void HandleAndReset(string msg) => Handle(msg, true);
+
+    void Handle(string msg, bool doResetGit)
+    {
+        _logger.Error(msg);
+        if (doResetGit)
         {
-            _logger = logger;
-            _git = git;
-            _runtime = runtime;
+            _logger.Error("...running git reset to undo any changes...");
+            _git.Value.RollbackAllChanges();
         }
 
-        public void Handle(string msg) => Handle(msg, false);
-
-        public void Handle(string tool, string arguments)
-        {
-            Handle($"call '{tool} {arguments}' failed - aborting", true);
-        }
-
-        public void HandleAndReset(string msg) => Handle(msg, true);
-
-        void Handle(string msg, bool doResetGit)
-        {
-            _logger.Error(msg);
-            if (doResetGit)
-            {
-                _logger.Error("...running git reset to undo any changes...");
-                _git.Value.RollbackAllChanges();
-            }
-
-            _runtime.Abort();
-        }
+        _runtime.Abort();
     }
 }
