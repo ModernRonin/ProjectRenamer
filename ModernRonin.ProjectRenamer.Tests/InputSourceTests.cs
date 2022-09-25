77d32fe4ffdb07223cabc561a95e5768b207cf32
Mark Hajek <markhajek@outlook.com>
2022-09-25 13:29:53 +0100

    test coverage for InputSource

---
+++ ModernRonin.ProjectRenamer.Tests/InputSourceTests.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.IO;
+using AutofacContrib.NSubstitute;
+using FluentAssertions;
+using ModernRonin.FluentArgumentParser.Help;
+using ModernRonin.FluentArgumentParser.Parsing;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace ModernRonin.ProjectRenamer.Tests;
+
+[TestFixture]
+public class InputSourceTests
+{
+    [SetUp]
+    public void Setup()
+    {
+        _dependencies = AutoSubstitute.Configure()
+            .Provide(new[]
+            {
+                "a1",
+                "a2"
+            })
+            .Build();
+        _underTest = _dependencies.Resolve<InputSource>();
+    }
+
+    AutoSubstitute _dependencies;
+    InputSource _underTest;
+    string[] CommandLineArguments => _dependencies.Resolve<string[]>();
+    IFilesystem Filesystem => _dependencies.Resolve<IFilesystem>();
+    IBindingCommandLineParser Parser => _dependencies.Resolve<IBindingCommandLineParser>();
+
+    [Test]
+    public void Get_returns_the_full_absolute_path_to_the_solution_file()
+    {
+        // arrange
+        Filesystem.FindSolutionFiles(".", false)
+            .Returns(new[] { "s1.sln" });
+        var userVerb = new Verb
+        {
+            OldProjectName = "a.b.csproj",
+            NewProjectName = "changed.b.csproj",
+            ProjectFileExtension = ".csproj"
+        };
+        Parser.Parse(CommandLineArguments)
+            .Returns(userVerb);
+        // act
+        var (verb, _) = _underTest.Get();
+        verb.Should().BeSameAs(userVerb);
+        verb.OldProjectName.Should().Be("a.b");
+        verb.NewProjectName.Should().Be("changed.b");
+    }
+
+    [Test]
+    public void Get_returns_the_verb_with_extensions_removed_from_project_names()
+    {
+        // arrange
+        Filesystem.FindSolutionFiles(".", false)
+            .Returns(new[] { "./tmp/s1.sln" });
+        var userVerb = new Verb
+        {
+            OldProjectName = "a.b.csproj",
+            NewProjectName = "changed.b.csproj",
+            ProjectFileExtension = ".csproj"
+        };
+        Parser.Parse(CommandLineArguments)
+            .Returns(userVerb);
+        // act
+        var (_, solutionPath) = _underTest.Get();
+        solutionPath.Should()
+            .Be(Path.Combine(Directory.GetCurrentDirectory(),
+                OperatingSystem.IsWindows() ? "tmp\\s1.sln" : "tmp/s1.sln"));
+    }
+
+    [Test]
+    public void Get_throws_if_the_given_source_project_name_seems_to_be_a_path_instead_of_a_name()
+    {
+        // arrange
+        Filesystem.FindSolutionFiles(".", false)
+            .Returns(new[] { "s1" });
+        Parser.Parse(CommandLineArguments)
+            .Returns(new Verb { OldProjectName = "a/b" });
+        // act
+        var action = () => _underTest.Get();
+        // assert
+        action.Should()
+            .Throw<AbortException>()
+            .Which.Message.Should()
+            .StartWith("Do not specify paths for input/'old' project names, please.");
+    }
+
+    [Test]
+    public void Get_throws_if_the_parser_returns_help([Values] bool isBecauseOfBadInput)
+    {
+        // arrange
+        Filesystem.FindSolutionFiles(".", false)
+            .Returns(new[] { "s1" });
+        Parser.Parse(CommandLineArguments)
+            .Returns(new HelpResult
+            {
+                IsResultOfInvalidInput = isBecauseOfBadInput,
+                Text = "bla"
+            });
+        // act
+        var action = () => _underTest.Get();
+        // assert
+        action.Should()
+            .Throw<AbortException>()
+            .WithMessage("bla")
+            .Which.ExitCode.Should()
+            .Be(isBecauseOfBadInput ? -1 : 0);
+    }
+
+    [Test]
+    public void Get_throws_if_the_parser_unexpected_result()
+    {
+        // arrange
+        Filesystem.FindSolutionFiles(".", false)
+            .Returns(new[] { "s1" });
+        Parser.Parse(CommandLineArguments)
+            .Returns("bla");
+        // act
+        var action = () => _underTest.Get();
+        // assert
+        action.Should()
+            .Throw<AbortException>()
+            .WithMessage(
+                "Something went seriously wrong. Please create an issue at https://github.com/ModernRonin/ProjectRenamer with as much detail as possible.");
+    }
+
+    [Test]
+    public void Get_throws_if_there_is_more_than_one_solution_in_the_current_directory()
+    {
+        // arrange
+        Filesystem.FindSolutionFiles(".", false)
+            .Returns(new[]
+            {
+                "s1",
+                "s2"
+            });
+        // act
+        var action = () => _underTest.Get();
+        // assert
+        action.Should()
+            .Throw<AbortException>()
+            .WithMessage("Needs to be run from a directory with exactly one *.sln file in it.");
+    }
+
+    [Test]
+    public void Get_throws_if_there_is_no_solution_in_the_current_directory()
+    {
+        // arrange
+        Filesystem.FindSolutionFiles(".", false).Returns(Array.Empty<string>());
+        // act
+        var action = () => _underTest.Get();
+        // assert
+        action.Should()
+            .Throw<AbortException>()
+            .WithMessage("Needs to be run from a directory with exactly one *.sln file in it.");
+    }
+}
--- ModernRonin.ProjectRenamer/InputSource.cs
+++ ModernRonin.ProjectRenamer/InputSource.cs
@@ -11,14 +11,11 @@ public class InputSource : IInputSource
     readonly string[] _commandLineArguments;
     readonly IFilesystem _filesystem;
     readonly IBindingCommandLineParser _parser;
-    readonly IRuntime _runtime;
 
-    public InputSource(IRuntime runtime,
-        IFilesystem filesystem,
+    public InputSource(IFilesystem filesystem,
         IBindingCommandLineParser parser,
         string[] commandLineArguments)
     {
-        _runtime = runtime;
         _filesystem = filesystem;
         _parser = parser;
         _commandLineArguments = commandLineArguments;
@@ -29,29 +26,28 @@ public class InputSource : IInputSource
         var solutionFiles = _filesystem.FindSolutionFiles(".", false);
         if (1 != solutionFiles.Length)
             throw new AbortException("Needs to be run from a directory with exactly one *.sln file in it.");
-        var solutionPath = Path.GetFullPath(solutionFiles.First());
-        switch (_parser.HelpOverview, _parser.Parse(_commandLineArguments))
+        var parseResult = _parser.Parse(_commandLineArguments);
+        if (parseResult is HelpResult help)
+            throw new AbortException(help.Text, exitCode: help.IsResultOfInvalidInput ? -1 : 0);
+
+        if (parseResult is not Verb verb)
         {
-            case (_, HelpResult help):
-                _runtime.Info(help.Text);
-                throw new AbortException(help.IsResultOfInvalidInput ? -1 : 0);
-            case (var helpOverview, Verb verb):
-                if (verb.OldProjectName.Any(CommonExtensions.IsDirectorySeparator))
-                {
-                    throw new AbortException(
-                        $"Do not specify paths for input/'old' project names, please.{Environment.NewLine}{Environment.NewLine}{helpOverview}");
-                }
-
-                verb.OldProjectName = RemoveProjectFileExtension(verb.OldProjectName,
-                    verb.ProjectFileExtension);
-                verb.NewProjectName = RemoveProjectFileExtension(verb.NewProjectName,
-                    verb.ProjectFileExtension);
-
-                return new UserInput(verb, solutionPath);
-            default:
-                throw new AbortException(
-                    "Something went seriously wrong. Please create an issue at https://github.com/ModernRonin/ProjectRenamer with as much detail as possible.");
+            throw new AbortException(
+                "Something went seriously wrong. Please create an issue at https://github.com/ModernRonin/ProjectRenamer with as much detail as possible.");
         }
+
+        if (verb.OldProjectName.Any(CommonExtensions.IsDirectorySeparator))
+        {
+            throw new AbortException(
+                $"Do not specify paths for input/'old' project names, please.{Environment.NewLine}{Environment.NewLine}{_parser.HelpOverview}");
+        }
+
+        verb.OldProjectName = RemoveProjectFileExtension(verb.OldProjectName,
+            verb.ProjectFileExtension);
+        verb.NewProjectName = RemoveProjectFileExtension(verb.NewProjectName,
+            verb.ProjectFileExtension);
+
+        return new UserInput(verb, Path.GetFullPath(solutionFiles.First()));
     }
 
     static string RemoveProjectFileExtension(string projectName, string projectFileExtension) =>
