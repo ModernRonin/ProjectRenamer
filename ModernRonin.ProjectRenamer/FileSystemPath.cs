f60d06942b57b5468c8fea151b4063396808c7b5
Mark Hajek <markhajek@outlook.com>
2022-09-26 18:00:54 +0100

    proof-of-concept

---
+++ ModernRonin.ProjectRenamer/FileSystemPath.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.IO;
+using System.Linq;
+
+namespace ModernRonin.ProjectRenamer;
+
+public sealed record FileSystemPath
+{
+    readonly ImmutableArray<string> _segments;
+
+    public FileSystemPath(string path) : this(path.Split(new[]
+    {
+        Path.DirectorySeparatorChar,
+        Path.AltDirectorySeparatorChar
+    }, StringSplitOptions.RemoveEmptyEntries)) { }
+
+    FileSystemPath(IEnumerable<string> segments) => _segments = segments.ToImmutableArray();
+
+    public override string ToString()
+    {
+        var separator = IsWindowsSpecific ? Path.DirectorySeparatorChar : Path.AltDirectorySeparatorChar;
+        return string.Join(separator, _segments);
+    }
+
+    /// <summary>
+    ///     Necessary to support indexing.
+    /// </summary>
+
+    public FileSystemPath this[int index] => new(ImmutableArray<string>.Empty.Add(_segments[index]));
+
+    public FileSystemPath Append(FileSystemPath other) => new(_segments.AddRange(other._segments));
+
+    /// <summary>
+    ///     Necessary to support ranges.
+    /// </summary>
+    public FileSystemPath Slice(int start, int length)
+    {
+        var range = _segments.RemoveRange(start + length, _segments.Length - start - length)
+            .RemoveRange(0, start);
+        return new FileSystemPath(range);
+    }
+
+    public FileSystemPath WithExtension(string extension)
+    {
+        if (!extension.StartsWith(".")) extension = $".{extension}";
+        var last = _segments[^1];
+        var replacement = replaceExtension(last, extension);
+
+        return new FileSystemPath(_segments.SetItem(_segments.Length - 1, replacement));
+
+        string replaceExtension(string s, string newExtension)
+        {
+            if (s.EndsWith(newExtension)) return s;
+            var i = s.LastIndexOf('.');
+            return i < 0 ? s + newExtension : s[..i] + newExtension;
+        }
+    }
+
+    public bool ContainsDirectory => _segments.Length > 1;
+    public static FileSystemPath CurrentDirectory => new(Path.GetFullPath(Directory.GetCurrentDirectory()));
+    public static FileSystemPath Empty { get; } = new(Enumerable.Empty<string>());
+
+    public bool HasExtension => _segments[^1].Contains(".");
+    public bool IsEmpty => _segments.Length == 0;
+
+    public bool IsWindowsSpecific => _segments.First().Contains(Path.VolumeSeparatorChar);
+
+    /// <summary>
+    ///     Necessary to support indexing.
+    /// </summary>
+    public int Length => _segments.Length;
+
+    public FileSystemPath Parent => ContainsDirectory ? this[..^1] : Empty;
+}
--- ModernRonin.ProjectRenamer/Project.cs
+++ ModernRonin.ProjectRenamer/Project.cs
@@ -1,5 +1,4 @@
 ﻿using System.IO;
-using System.Linq;
 
 namespace ModernRonin.ProjectRenamer;
 
@@ -10,13 +9,23 @@ public sealed record Project(string FullPath, string SolutionFolder, string Exte
 {
     public Project Rename(string newName, string currentDirectory)
     {
-        var newBaseDir = newName.Any(CommonExtensions.IsDirectorySeparator)
-            ? currentDirectory
-            : Path.GetDirectoryName(Directory);
-        var newDir = newName.ToAbsolutePath(newBaseDir);
-        var newFileName = Path.GetFileName(newName);
-        var newProjectPath = Path.Combine(newDir, $"{newFileName}{Extension}");
-        return this with { FullPath = newProjectPath };
+        var p = new FileSystemPath(FullPath);
+        var cd = new FileSystemPath(currentDirectory);
+        var nn = new FileSystemPath(newName);
+        var d = new FileSystemPath(Directory);
+
+        var newBase = nn.ContainsDirectory ? cd : d.Parent;
+        var newFn = nn[^1].WithExtension(Extension);
+        var npp = newBase.Append(nn).Append(newFn);
+        return this with { FullPath = npp.ToString() };
+
+        //var newBaseDir = newName.Any(CommonExtensions.IsDirectorySeparator)
+        //    ? currentDirectory
+        //    : Path.GetDirectoryName(Directory);
+        //var newDir = newName.ToAbsolutePath(newBaseDir);
+        //var newFileName = Path.GetFileName(newName);
+        //var newProjectPath = Path.Combine(newDir, $"{newFileName}{Extension}");
+        //return this with { FullPath = newProjectPath };
     }
 
     public string Directory => Path.GetDirectoryName(FullPath);
